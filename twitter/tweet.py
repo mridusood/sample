199d8a944eacd17c5e07cbf9e92511cdaa0eba38
mridusood <richasood120@gmail.com>
2018-06-30 19:44:17 +0530

    first commit

---
+++ twitter/tweet.py
@@ -0,0 +1,11 @@
+from key import Consumer_Key,Consumer_Secret,Access_Token,AccessToken_Secret
+import tweepy
+
+oauth=tweepy.OAuthHandler(Consumer_Key,Consumer_Secret)
+oauth.set_access_token(Access_Token,AccessToken_Secret)
+api=tweepy.API(oauth)
+print(api.search("#modi"))
+
+user=api.get_user('richasood')
+print(user.screen_name)
+print(user.followers_count)
